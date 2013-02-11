24cb34449adf4ece2dbb096d3a2b22df3b36fff8
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-02-11 01:04:59 +0100

    fixtures of some sort (well, sql inserts at least)

---
+++ data/hops.sql
@@ -0,0 +1,23 @@
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("British Columbia Goldings", 5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Cascade", 5.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Crystal",3.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("East Kent Goldings",5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Fuggles",4);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Glacier",5.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Hallertauer Hersbrucker",3.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Hallertauer Mittelfrüh",4);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Liberty",4);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Mt. Hood",6);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Progress",5.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Saaz",3.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Spalt",4.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Styrian Goldings",6);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Tettnang",4.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Willamette",5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Whitbread Goldings Variety",4.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Challenger",8.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Summit",18.5);
+INSERT INTO hop('name', 'alpha_acid' ) VALUES ("Centennial",7.8);
+
+
+--lifted from "how to brew"
---
+++ data/malts.sql
@@ -0,0 +1,27 @@
+INSERT INTO malt('name', 'ppg') VALUES ("2 Row Lager Malt ", 37);
+INSERT INTO malt('name', 'ppg') VALUES ("6 Row Base Malt", 35);
+INSERT INTO malt('name', 'ppg') VALUES ("2 Row Pale Ale Malt", 38);
+INSERT INTO malt('name', 'ppg') VALUES ("Biscuit/Victory Malt ", 35);
+INSERT INTO malt('name', 'ppg') VALUES ("Vienna Malt", 35);
+INSERT INTO malt('name', 'ppg') VALUES ("Munich Malt", 35);
+INSERT INTO malt('name', 'ppg') VALUES ("Brown Malt ", 32);
+INSERT INTO malt('name', 'ppg') VALUES ("Dextrin Malt ", 32);
+INSERT INTO malt('name', 'ppg') VALUES ("Light Crystal (10 - 15L) ", 35);
+INSERT INTO malt('name', 'ppg') VALUES ("Pale Crystal (25 - 40L)", 34);
+INSERT INTO malt('name', 'ppg') VALUES ("Medium Crystal (60 - 75L)", 34);
+INSERT INTO malt('name', 'ppg') VALUES ("Dark Crystal (120L)", 33);
+INSERT INTO malt('name', 'ppg') VALUES ("Special B", 31);
+INSERT INTO malt('name', 'ppg') VALUES ("Chocolate Malt ", 28);
+INSERT INTO malt('name', 'ppg') VALUES ("Roast Barley ", 25);
+INSERT INTO malt('name', 'ppg') VALUES ("Black Patent Malt", 25);
+INSERT INTO malt('name', 'ppg') VALUES ("Wheat Malt ", 37);
+INSERT INTO malt('name', 'ppg') VALUES ("Rye Malt ", 29);
+INSERT INTO malt('name', 'ppg') VALUES ("Oatmeal (Flaked) ", 32);
+INSERT INTO malt('name', 'ppg') VALUES ("Corn (Flaked)", 39);
+INSERT INTO malt('name', 'ppg') VALUES ("Barley (Flaked)", 32);
+INSERT INTO malt('name', 'ppg') VALUES ("Wheat (Flaked) ", 36);
+INSERT INTO malt('name', 'ppg') VALUES ("Rice (Flaked)", 38);
+INSERT INTO malt('name', 'ppg') VALUES ("Malto - Dextrin Powder ", 40);
+INSERT INTO malt('name', 'ppg') VALUES ("Sugar (Corn, Cane) ", 46);
+
+--taken from http://www.howtobrew.com/section2/chapter12-4-1.html
