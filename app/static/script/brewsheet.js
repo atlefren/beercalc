e15209235704115bf62c29784a4a00af28980364
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-02-23 19:50:05 +0100

    save confirm alert

--- app/static/script/brewsheet.js
+++ app/static/script/brewsheet.js
@@ -986,6 +986,8 @@ var ol = {};
 
         saved: function() {
             window.history.pushState("object or string", "Title", "/brews/" + this.brew.get("id"));
+
+            this.$el.append(_.template($("#success_alert_template").html(), {"type": "success", "message": "Brew saved!"}));
         },
 
         clone: function() {
